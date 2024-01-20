ab52e680f1149d05c38379fb421ad15169c4cb48
Guilherme Sanches <guitdsanches@gmail.com>
2024-01-20 12:46:30 -0300

    test(eventos): adicionando testes para a rota /eventos

--- src/models/evento.js
+++ src/models/evento.js
@@ -29,6 +29,14 @@ class Evento {
       autor_id: 1,
       created_at: '2023-01-01 08:00:00',
       updated_at: '2023-01-01 08:00:00',
+    }, {
+      id: 2,
+      nome: 'Evento Teste 2',
+      descricao: 'Descrição teste',
+      data: '2023-01-01',
+      autor_id: 1,
+      created_at: '2023-01-01 08:00:00',
+      updated_at: '2023-01-01 08:00:00',
     }];
   }
 }
